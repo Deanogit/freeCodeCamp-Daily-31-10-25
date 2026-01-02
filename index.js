28cff7d86c2bc829708116fcec020818b9b282ef
Dean Howe <hideanhowe@gmail.com>
2026-01-02 07:10:55 +0000

    testing

--- index.js
+++ index.js
@@ -4,3 +4,33 @@
 // Replace all underscores (_), and hyphens (-) with a tilde (~).
 // Capitalize the first letter of the string, and every other letter after that. Ignore the tilde character when counting. Make all other letters lowercase.
 // For example, given hello_world, return HeLlO~wOrLd.
+
+function spookify(boo) {
+  console.log(boo);
+
+  // handletilde
+  const tildeRegex = /[_-]/;
+  let tilde = boo.replace(tildeRegex, '~');
+  console.log(tilde);
+
+  // split
+  const arr = tilde.split('');
+
+  let letterRegex = /[a-z]/i;
+  let counter = 0;
+  // loop
+  for (let i = 0; i < arr.length; i++) {
+    if (letterRegex.test(arr[i])) {
+      counter++;
+    }
+    if (counter === 0 || counter % 2 === 0) {
+      arr[i] = arr[i].toUpperCase();
+    } else {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  console.log(arr.join(''));
+  return arr.join('');
+
+  return boo;
+}
